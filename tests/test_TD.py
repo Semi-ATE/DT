8b8a6f61886c2136d5b475482b10515324ebd475
Tom Hören <horen.tom@gmail.com>
2021-02-23 01:57:35 +0100

    removed dummy assert

--- tests/test_TD.py
+++ tests/test_TD.py
@@ -16,4 +16,4 @@ two = 2 * one
 
 assert TD(one).__str__() == "1 year 1 month 1 week 1 day 1 hour 1 minute 1 second"
 assert TD(two).__str__() == "2 years 2 months 2 weeks 2 days 2 hours 2 minutes 2 seconds"
-assert False
+
